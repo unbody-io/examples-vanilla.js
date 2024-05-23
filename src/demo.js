5c609b8cdcbab3c36862fe9dd3805bc21af75742
amirhouieh <amir.houieh@gmail.com>
2024-05-23 12:12:27 +0200

    add visual search

--- src/demo.js
+++ src/demo.js
@@ -9,9 +9,16 @@ const generativeInputContext = document.getElementById("generative-input-context
 const generativeResults = document.getElementById("generative-results");
 const generativeForm = document.getElementById("generative-form");
 
-const visualSearchInput = document.getElementById("visual-search-input");
-const visualSearchResults = document.getElementById("visual-search-results");
-const visualSearchForm = document.getElementById("visual-search-form");
+const visualSimilarityInput = document.getElementById("visual-similarity-input");
+const visualSimilarityResults = document.getElementById("visual-similarity-results");
+const visualSimilarityForm = document.getElementById("visual-similarity-form");
+
+const visualSearchInput = document.getElementById("visual-search-input")
+const visualSearchResults = document.getElementById("visual-search-results")
+const visualSearchForm = document.getElementById("visual-search-form")
+
+const visualBtnSearch = document.getElementById("visual-search-button")
+const visualBtnGenerative = document.getElementById("visual-search-generate-button")
 
 
 const unbody = new Unbody({
@@ -20,11 +27,95 @@ const unbody = new Unbody({
 });
 
 
+
+const imageCard = ({url, originalName, autoTypes}) => {
+    if(!autoTypes)  return "";
+
+    return `<div class="rounded-2xl max-h-[300px] overflow-hidden">
+                <img src="${url}?w=300"/>
+                <small class="text-xs text-gray-500">...${originalName}</small>
+                <span class="text-xs">${autoTypes? autoTypes:""}</span>
+           </div>`
+}
+
 (async () => {
-    console.log("start");
-    const s = await unbody.get.googleDoc.select("title").exec();
-    console.log(s)
-})();
+   const {data: {payload}} = await unbody.get
+       .imageBlock
+       .select("autoCaption", "autoOCR","autoTypes", "url", "originalName")
+       .exec();
+   visualSearchResults.innerHTML = `
+     <div class="grid grid-cols-6 gap-2">
+        ${payload.map(imageCard).join("")}
+     </div>`
+})()
+
+
+
+visualBtnSearch.addEventListener("click", async (e) => {
+    const query = visualSearchInput.value;
+    visualSearchResults.innerText = "Loading..."
+
+    const {data: {payload}} = await unbody.get
+        .imageBlock
+        .select("autoCaption", "autoOCR","autoTypes", "url")
+        .search
+        .about(query, { certainty: 0.65})
+        .exec();
+
+    if (payload.length === 0) {
+        visualSearchResults.innerText = "No results found"
+        return
+    }
+
+    visualSearchResults.innerHTML = `
+        <div class="rounded-2xl max-h-[400px] mr-auto ml-auto max-w-screen-md overflow-hidden mb-6 mt-6">
+            <img src="${payload[0].url}?w=600" style="width: 100%">
+        </div>
+        <div class="grid grid-cols-6 gap-2">
+            ${payload.slice(1, -1).map(imageCard).join("")}
+        </div>
+    `
+});
+
+
+visualBtnGenerative.addEventListener("click", async (e) => {
+    const query = visualSearchInput.value;
+    visualSearchResults.innerText = "Loading..."
+
+    const {data: {payload, generate}} = await unbody.get
+        .imageBlock
+        .select("autoCaption", "autoOCR","autoTypes", "url")
+        .search
+        .about(query)
+        .limit(2)
+        .generate
+        .fromMany(
+            `${query}`,
+            ["autoCaption", "autoOCR", "autoTypes"]
+        )
+        .exec();
+
+    visualSearchResults.innerText = ""
+    if (generate) {
+        visualSearchResults.innerHTML += `<div class="whitespace-pre max-w-screen-md p-6">${generate.result}</div>`
+    }
+
+    visualSearchResults.innerHTML += `
+        <div class="grid grid-cols-6 gap-2">
+            ${payload.map(imageCard).join("")}
+        </div>
+    `
+});
+
+
+
+
+
+
+
+
+
+
 
 const search = async (e) => {
     e.preventDefault();
@@ -32,17 +123,21 @@ const search = async (e) => {
 
     searchResults.innerText = "Loading..."
 
-    //
-    // const {data} = await unbody
-    //     .get
-    //     .textBlocks
-    //      .search(query)
-    //     .exec();
+    const {data: {payload}} = await unbody
+        .get
+        .googleDoc
+        .select("title", "autoTopics")
+        .search
+        .about(query)
+        .exec();
 
-    // console.log(data)
+    console.log(payload)
 
-    // Fetch data from Unbody
-    // set searchResults
+    searchResults.innerHTML = ""
+    payload.forEach(({title, autoTopics}) => {
+        searchResults.innerHTML += `<li>${title}}</li>`
+    })
+
 }
 
 
@@ -54,22 +149,28 @@ const generative = async (e) => {
 
     generativeResults.innerText = "Loading..."
 
-    const {data} = await unbody.get
-        .imageBlock
-        .similar
+    const {data: {generate}} = await unbody.get
+        .googleDoc
+        .search
+        .about(context)
+        .limit(2)
+        .select("title", "autoSummary")
+        .generate
+        .fromMany(
+            prompt,
+            ["title", "autoSummary", "autoTopics"]
+        )
+        .exec()
 
-
-    // Fetch data from Unbody
-
-    // set generativeResults
+    generativeResults.innerText = generate.result;
 }
 
-const visualSearch = async (e) => {
+const visualSimilarity = async (e) => {
     e.preventDefault();
 
-    const url = visualSearchInput.value;
+    const url = visualSimilarityInput.value;
 
-    visualSearchResults.innerHTML = `<img src="${url}" style="width: 200px" />`
+    visualSimilarityResults.innerHTML = `<img src="${url}" style="width: 200px" />`
 
     const {data: {payload}} = await unbody.get
         .imageBlock.similar
@@ -77,13 +178,13 @@ const visualSearch = async (e) => {
         .select("url")
         .exec();
 
-    visualSearchResults.innerHTML += payload.map(img => `<img src="${img.url}" style="width: 200px" class="pt-2" />`).join("")
+    visualSimilarityResults.innerHTML += payload.map(img => `<img src="${img.url}" style="width: 200px" class="pt-2" />`).join("")
     // Fetch data from Unbody
-    // set visualSearchResults
+    // set visualSimilarityResults
 }
 
 
 searchForm.addEventListener("submit", search, true);
 generativeForm.addEventListener("submit", generative, true);
-visualSearchForm.addEventListener("submit", visualSearch, true);
+visualSimilarityForm.addEventListener("submit", visualSimilarity, true);
 
